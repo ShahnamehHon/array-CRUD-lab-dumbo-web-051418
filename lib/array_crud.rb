c44e6cde4fb2aa90c17e7e054ecc39c304f8abb4
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-08 18:29:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["A", "B", "C", "E"]
+  array = ["A", "B", "am", "E"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index ["C"]
+  array.index ["am"]
 end
 
 def retrieve_first_element_from_array(array)
